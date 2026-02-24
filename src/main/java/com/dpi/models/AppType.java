b5f01340ed5f5ecab4c8e485210e55b98041e8a6
Harshicfai <131523506+Harshicfai@users.noreply.github.com>
2026-02-25 04:16:29 +0530

    Add AppType enum file

---
+++ src/main/java/com/dpi/models/AppType.java
@@ -0,0 +1,18 @@
+package com.dpi.models;
+
+public enum AppType {
+    UNKNOWN,
+    HTTP,
+    HTTPS,
+    DNS,
+    GOOGLE,
+    YOUTUBE,
+    FACEBOOK,
+    TIKTOK,
+    TWITTER,
+    GITHUB,
+    NETFLIX,
+    INSTAGRAM,
+    WHATSAPP,
+    TELEGRAM
+}
